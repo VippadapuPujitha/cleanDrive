75060d9db086c37f9e4dd8eccdb6ecc7d1121c6b
VippadapuPujitha <vippadapupujitha12@gmail.com>
2025-07-31 23:43:32 +0530

    Add files via upload

---
+++ login.py
@@ -0,0 +1,15 @@
+ # login.py
+
+# Predefined username and password
+USERNAME = "admin"
+PASSWORD = "1234"
+
+# Ask for user input
+input_username = input("Enter username: ")
+input_password = input("Enter password: ")
+
+# Check credentials
+if input_username == USERNAME and input_password == PASSWORD:
+    print("✅ Login successful!")
+else:
+    print("❌ Invalid username or password.")
